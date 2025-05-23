a5bafaf059ed078afc79d75d6c27ce0d0ac9d16b
Amir <naby1371@gmail.com>
2025-05-23 19:13:08 +0330

    Initial Commit

--- src/main/java/com/niam/kardan/KardanApplication.java
+++ src/main/java/com/niam/kardan/KardanApplication.java
@@ -2,8 +2,10 @@ package com.niam.kardan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.niam")
 public class KardanApplication {
     public static void main(String[] args) {
         SpringApplication.run(KardanApplication.class, args);
